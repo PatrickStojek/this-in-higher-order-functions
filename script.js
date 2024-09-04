3ab31c8caa6a29e4f6b08ff3823d63191c05b3e1
PatrickStojek <patryk.stojek7@gmail.com>
2024-09-04 20:47:55 +0200

    add greeter function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log('Hello world!')
 const myOwnForEach = function(array, callback, thisArg) {
     for(let i = 0; i < array.length; i++) {
         const element = names[i];
@@ -9,3 +10,13 @@ const myOwnForEach = function(array, callback, thisArg) {
 }
 
 const names = ['Ola', 'Ala', 'Ela']
+
+window.myName = 'Patryk'
+
+const greeter = function (name, index, array) {
+    console.log('Hello ' + name + ' I\'m ' + this.myName)
+}
+
+names.forEach(greeter)
+myOwnForEach(names, greeter)
+
