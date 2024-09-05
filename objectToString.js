016956ee51b02520afe7e72cbe3528fe8e177050
PatrickStojek <patryk.stojek7@gmail.com>
2024-09-05 18:52:35 +0200

    add objectToString function

---
+++ objectToString.js
@@ -0,0 +1,17 @@
+const objectToString = {
+    firstName: 'Object',
+    lastName: 'Stringified',
+    toString: function() {
+        let string = ''
+        for(const property in this) {
+            const value = this[property]
+            if(typeof value === 'function') {
+                continue
+            }
+            string = string + ' ' + value
+        }
+        return string
+    }
+}
+
+console.log(objectStringify.toString())
